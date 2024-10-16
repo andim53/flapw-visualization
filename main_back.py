f29a89e633325c47d2fc5d02a7a7c2dc2f0a9e25
Andi <andimuhammad164@gmail.com>
2024-10-16 11:39:51 +0900

    New commit

--- main_back.py
+++ main_back.py
@@ -1,19 +1,27 @@
+#!/usr/bin/env python
 
+from __future__ import print_function
+
+import os, re
 import numpy as np
+from ase.io import read
 from optparse import OptionParser
 
 
 import matplotlib as mpl
 
+mpl.use('agg')
+mpl.rcParams['axes.unicode_minus'] = False
+
+'''
+mpl.use('agg'): Configures matplotlib to use a file-based backend, rendering plots directly into image files rather than displaying them interactively.
+mpl.rcParams['axes.unicode_minus'] = False: Ensures that the ASCII minus sign (-) is used in plots, avoiding potential issues with Unicode characters.
+'''
+
 import matplotlib.pyplot as plt
 from matplotlib.ticker import AutoMinorLocator
 from matplotlib.patches import Polygon
 import matplotlib.colors as mcolors
-
-from time import time
-
-
-__version__ = "0.0.1"
 
 def command_line_arg():
     usage = "usage: %prog [options] arg1 arg2"
@@ -78,7 +86,9 @@ def command_line_arg():
                    action='store_false', dest='showtotal',
                    help='not show total dos')
     
+    
     return par.parse_args()
+
 
 def gradient_fill(x, y, fill_color=None, ax=None, direction=1, **kwargs):
     """
@@ -136,31 +146,11 @@ def gradient_fill(x, y, fill_color=None, ax=None, direction=1, **kwargs):
     ax.autoscale(True)
 
     return line, im
+############################################################
 
-def readDOSFromFile(opts):
-    '''
-    Read DOS info from file.
+    
 
-    the format of the DOS file:
-    first line: labels of the dos: # eng dos occup e_sum
-    '''
-    
-    inp = open(opts.dos).readlines()
-    labels = inp[0].split()[1:]
-    
-    # Data
-    DOS = np.array([line.split() for line in inp[1:] if line.strip()],
-                    dtype=float)
-    
-    xen = DOS[:, 0]
-    tdos = DOS[:, 1]
-    occ = DOS[:, 2]
-    e_sum = DOS[:,3]
-    
-    opts.pdosLabel = labels
-
-    return xen, tdos, occ, e_sum
-
+# def dosplot(xen, tdos, pdos, opts):
 def dosplot(xen, tdos, opts):
     '''
     Use matplotlib to plot density of state
@@ -168,9 +158,9 @@ def dosplot(xen, tdos, opts):
 
     width, height = opts.figsize
     xmin, xmax = opts.xlim
+    dpi = opts.dpi
 
     plt.style.use(opts.mpl_style)
-    
     # DO NOT use unicode minus regardless of the style
     mpl.rcParams['axes.unicode_minus'] = False
 
@@ -179,26 +169,80 @@ def dosplot(xen, tdos, opts):
     ax = plt.subplot(111)
 
     LINES = []
+    # nspin = tdos.shape[1]
+    nspin = tdos.shape[0]
 
     plabels = []
+    LWs = []
+    LCs = []
+    
+    # Adding Projected Density of State, for each orbital
+    
+    # if opts.pdosAtom:
+    #     plabels = ['p_%d' % ii for ii in range(len(opts.pdosAtom))]
+    #     LWs = [0.5 for ii in range(len(opts.pdosAtom))]
+    #     LCs = [None for ii in range(len(opts.pdosAtom))]
+
+    #     for ii in range(min(len(opts.pdosAtom), len(opts.pdosLabel))):
+    #         plabels[ii] = opts.pdosLabel[ii]
+    #     for ii in range(min(len(opts.pdosAtom), len(opts.linewidth))):
+    #         LWs[ii] = opts.linewidth[ii]
+    #     for ii in range(min(len(opts.pdosAtom), len(opts.linecolors))):
+    #         LCs[ii] = opts.linecolors[ii]
 
     plabels += ['total']
 
-    xen -= opts.zero # energy reference of the band plot
+    xen -= opts.zero
+    # for ip, p in enumerate(pdos):
+    #     for ii in range(nspin):
+    #         fill_direction = 1 if ii == 0 else -1
+    #         lc = LCs[ip] if ii == 0 else line.get_color()
+
+    #         if opts.fill:
+    #             line, im = gradient_fill(xen, p[:, ii], ax=ax, lw=LWs[ip],
+    #                                      color=lc,
+    #                                      direction=fill_direction)
+    #         else:
+    #             line, = ax.plot(xen, p[:, ii], lw=LWs[ip], alpha=0.6,
+    #                             color=lc)
+    #         if ii == 0:
+    #             LINES += [line]
 
     if opts.showtotal:
-            
         line, im = gradient_fill(xen, tdos, ax=ax,
                                  color = 'black',
                                  lw=0.5
+                                 # zorder=-1
                                  )
         LINES += [line]
+        # for ii in range(nspin):
+        #     fill_direction = 1 if ii == 0 else -1
+            # lc = 'k' if ii == 0 else line.get_color()
 
-        ax.set_xlabel('Energy [eV]',  # fontsize='small',
-                      labelpad=5)
-        ax.set_ylabel('DOS [arb. unit]',  # fontsize='small',
-                      labelpad=10)
-            
+            # if opts.fill:
+            #     # line, im = gradient_fill(xen, tdos[:, ii], ax=ax,
+            #     #                          color=lc,
+            #     #                          lw=0.5,
+            #     #                          # zorder=-1,
+            #     #                          direction=fill_direction,
+            #     #                          )
+            #     ine, im = gradient_fill(xen, tdos[ii], ax=ax,
+            #                              lw=0.5,
+            #                              # zorder=-1,
+            #                              direction=fill_direction,
+            #                              )
+            # else:
+            #     # line, = ax.plot(xen, tdos[:, ii], color=lc,
+            #     #                 lw=0.5, alpha=0.6)
+            #     line, = ax.plot(xen, tdos[:, ii],
+            #                     lw=0.5, alpha=0.6)
+            # if ii == 0:
+            #     LINES += [line]
+
+    ax.set_xlabel('Energy [eV]',  # fontsize='small',
+                  labelpad=5)
+    ax.set_ylabel('DOS [arb. unit]',  # fontsize='small',
+                  labelpad=10)
     ax.tick_params(which='both', labelsize='small')
 
     ax.set_xlim(xmin, xmax)
@@ -206,9 +250,12 @@ def dosplot(xen, tdos, opts):
         ymin, ymax = opts.ylim
         ax.set_ylim(ymin, ymax)
 
+    # ax.set_yticklabels([])
+
     ax.xaxis.set_minor_locator(AutoMinorLocator(2))
     ax.yaxis.set_minor_locator(AutoMinorLocator(2))
 
+    opts.pdosLabel = plabels
     ax.legend(LINES, plabels,
               loc=opts.legendloc,
               fontsize='small',
@@ -218,18 +265,66 @@ def dosplot(xen, tdos, opts):
     plt.tight_layout(pad=0.50)
     plt.savefig(opts.dosimage, dpi=opts.dpi)
     
+def readDOSFromFile(opts):
+    '''
+    Read DOS info from file.
+
+    the format of the DOS file:
+    first line: labels of the dos: # eng dos occup e_sum
+    '''
+    
+    # This needs to change later
+    # inp = open("dos.xy").readlines() 
+    inp = open(opts.dos).readlines()
+    labels = inp[0].split()[1:]
+    
+    # Data
+    DOS = np.array([line.split() for line in inp[1:] if line.strip()],
+                    dtype=float)
+
+    # NoPdos = (DOS.shape[1] - 1) // nspin - 1
+
+    # tDOS = np.empty((nedos, nspin))
+    # pDOS = []
+    
+    xen = DOS[:, 0]
+    tDOS = DOS[:, 1]
+    
+    # for ii in range(nspin):
+    #     tDOS[:, ii] = DOS[:, (ii + 1) * (NoPdos + 1)]
+    # for pp in range(NoPdos):
+    #     tmp = []
+    #     for ii in range(nspin):
+    #         tmp += [DOS[:, (pp + 1) + ii * (NoPdos + 1)]]
+    #     pDOS += [np.array(tmp).T]
+
+    # opts.nedos = nedos
+    # opts.pdosAtom = ['' for x in range(NoPdos)]
+    opts.pdosLabel = labels
+
+    # return xen, tDOS, pDOS
+
+    return xen, tDOS
 
 
-# Read Commands Line
-opts, args = command_line_arg()
+if __name__ == '__main__':
+    from time import time
 
-xen, tdos, occ, e_sum = readDOSFromFile(opts)
+    __version__ = "1.0"
 
-t0 = time()
-dosplot(xen, tdos, opts)
-t1 = time()
+    # Read Commands Line
+    opts, args = command_line_arg()
 
-print('DOS plot completed! Time Used: %.2f [sec]' % (t1 - t0))
+    # xen, tdos, pdos = readDOSFromFile(opts)
+    
+    xen, tdos = readDOSFromFile(opts)
 
+    t0 = time()
+    # dosplot(xen, tdos, pdos, opts)
+    dosplot(xen, tdos, opts)
 
+    t1 = time()
+    print('DOS plot completed! Time Used: %.2f [sec]' % (t1 - t0))
+
+    
 
